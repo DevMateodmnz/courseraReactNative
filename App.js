ffe4a0382157f484b4f835c531c26aae78247ce8
Mateo <mateogabrieldominguez@gmail.com>
2025-07-05 16:18:11 -0300

    Refactor: Implement navigation stack and update dependencies

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StyleSheet, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import LittleLemonHeader from './components/LittleLemonHeader';
-import MenuItems from './components/MenuItems';
+import LoginScreen from './LoginScreen';
+import WelcomeScreen from './WelcomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <View style={styles.container}>
-        <LittleLemonHeader />
-        <MenuItems />
-        <NavigationComponent />
-      </View>
-      <View style={styles.footerContainer}>
-        <LittleLemonFooter />
-      </View>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <LittleLemonHeader />
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <LittleLemonFooter />
+        </View>
+      </NavigationContainer>
     </>
   );
 }
